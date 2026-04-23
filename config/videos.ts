6c7f6ee09772a0afd6afed702453056e3f375836
Tomas Paletta <147454117+tomasfpaletta@users.noreply.github.com>
2026-04-23 17:09:43 -0300

    admin: actualizar videos de la landing

--- config/videos.ts
+++ config/videos.ts
@@ -11,4 +11,19 @@ export type Video = {
   activo: boolean
 }
 
-export const videos: Video[] = [];
+export const videos: Video[] = [
+  {
+    "id": "vid-1776974480555-qdt4",
+    "titulo": "video test",
+    "descripcion": "test",
+    "url": "https://youtube.com/shorts/5xfvedg4_y0",
+    "activo": true
+  },
+  {
+    "id": "vid-1776974498012-in0e",
+    "titulo": "test2",
+    "descripcion": "test2",
+    "url": "https://youtube.com/shorts/UbjsdTfxOqc",
+    "activo": true
+  }
+];
